ee0e4fb8be3d76e7188b0d7d82494056018bbc26
Piyush Padia <piyush.padia2021@vitstudent.ac.in>
2025-02-23 04:52:20 +0530

    changes

--- client/task/src/components/Login.js
+++ client/task/src/components/Login.js
@@ -12,7 +12,12 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://sooru-ai.onrender.com/api/auth/login", formData, { withCredentials: true, } );
+      const res = await axios.post("https://sooru-ai.onrender.com/api/auth/login", formData,   {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true, 
+      } );
       localStorage.setItem("token", res.data.token);
       navigate("/home");
     } catch (err) {
--- server/app.js
+++ server/app.js
@@ -10,11 +10,14 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://sooru-ai-ftwp.vercel.app/"],
+    origin: ["http://localhost:3000", "https://sooru-ai-ftwp.vercel.app"],
     methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"], 
     credentials: true, 
   })
 );
+
+app.options("*", cors());
 
 
 mongoose
